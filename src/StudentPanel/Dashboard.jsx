cd86070f0cc01b52a92fe97dde8e9ea5b9ad562d
SAHARIARSUPTO <supto622@gmail.com>
2024-05-20 08:17:18 +0600

    v1.1

--- src/StudentPanel/Dashboard.jsx
+++ src/StudentPanel/Dashboard.jsx
@@ -1,19 +1,15 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../Home/Navbar";
 import Sidebar from "./Sidebar";
-import Footer from "../Footer/Footer";
 
 const Dashboard = () => {
   return (
     <div>
-      <Navbar />
       <div style={{ display: "flex" }}>
         <Sidebar />
         <div>
           <Outlet></Outlet>
         </div>
       </div>
-      <Footer></Footer>
     </div>
   );
 };
